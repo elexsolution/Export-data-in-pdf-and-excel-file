faaf966f70e36e4f985dbed56e89ea08414c441f
Elexsolution <project@elexsolution.com>
2019-05-01 06:44:34 +0530

    Elexsolution
    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -54,7 +54,7 @@ $mpdf->setFooter('{PAGENO}/{nbpg}');
 			<meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
             <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-            <title>PLD User List</title>
+            <title>Store User List</title>
             <link rel='stylesheet' href='http://localhost/pdfcode/css/style.css'>
 			</head>
 		<body>
